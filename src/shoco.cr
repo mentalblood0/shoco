4f358d927a6926f7f8d05bbef4458127775b8f38
mentalblood0 <necheporenko.stepan@yandex.ru>
2025-08-11 12:59:04 +0300

    fixes

--- src/shoco.cr
+++ src/shoco.cr
@@ -10,15 +10,15 @@ module Shoco
       w = LibShoco.compress input.to_unsafe, input.size, r.to_unsafe, r.size
       return nil if w == r.size + 1
       raise Exception.new "shoco_compress(#{input.to_unsafe}, #{input.size}, #{r.to_unsafe}, #{r.size}) returned #{w}" if w > r.size
-      r
+      w > 0 ? r[..w - 1] : "".to_slice
     end
 
     def self.decompress(input : Bytes) : String
       return "" if input.empty?
       r = Bytes.new input.size * 2
-      w = LibShoco.decompress input.to_unsafe, input.size, r.to_unsafe, r.size
+      w = LibShoco.decompress input.to_unsafe, input.bytesize, r.to_unsafe, r.size
       raise Exception.new "shoco_compress(#{input.to_unsafe}, #{input.size}, #{r.to_unsafe}, #{r.size}) returned #{w}" if w > r.size
-      String.new r.to_unsafe
+      String.new r[..w - 1]
     end
   end
 end
